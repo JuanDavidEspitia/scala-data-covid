ca1c9e379141140b9b82a86140e6f3f049f20562
Juan David Espitia <juandespitia14@gmail.com>
2020-08-07 18:06:05 -0500

    Se agrego el dataset y se genero codigo nuevo

--- src/main/scala/com/sparkCovid/DatosCovid.scala
+++ src/main/scala/com/sparkCovid/DatosCovid.scala
@@ -1,7 +1,8 @@
 package com.sparkCovid
 
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
 import org.apache.log4j.{Level, Logger}
+
 
 object DatosCovid
 {
@@ -10,14 +11,27 @@ object DatosCovid
   {
     // El metodo Logger solo permite mostrar en pantalla lineas de tipo ERROR o WARN si es el caso
     Logger.getLogger("org").setLevel(Level.ERROR)
-    val conf = new SparkConf().setAppName("Datos_Covid").setMaster("local[*]")
-    val sc = new SparkContext(conf)
+    // declaramos las variables para el entorno de procesamiento spark sql
+    val session = SparkSession.builder().appName("Datos Covid").master("local[*]").getOrCreate()
 
-    println("Hello Scala Program")
-    println("Hello My Name is Juan David")
+    // Fuente: https://www.datos.gov.co/Salud-y-Protecci-n-Social/Casos-positivos-de-COVID-19-en-Colombia/gt2j-8ykr/data
 
+    // Cargamos el archivo .csv en un dataframe
+    val datosCovid = session.read
+      .option("header", "true")
+      .option("inferSchema", value = true)
+      .csv("input/datos-covid-col.csv")
+
+    // Imprimimos el dataframe
+    datosCovid.show()
+
+    println("---------------------    Cantidad de registros   -------------------")
+    println(datosCovid.count())
+
+    println("---------------  Imprimimos la estructura del DataFrame   ---------------")
+    // Imprimimos la estructura del DataFrame
+    datosCovid.printSchema()
 
   }
-
 
 }
